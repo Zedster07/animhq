e6a9469d2fd63aa6c533beb3713b454da18eadec
Admin <admin@mb-dz.bis>
2023-05-25 17:24:31 +0100

    latest update 250523

--- page.php
+++ page.php
@@ -40,46 +40,67 @@ $pages = array("contactus", "plans");
 
 		<?php
 		} else if ($post_slug == "plans") {
-			?>
-				
-				<div class="plansContainer">
+			global $wpdb;
+			$db_name = $wpdb->dbname;
+			$db_user = $wpdb->dbuser;
+			$db_password = $wpdb->dbpassword;
+			$db_host = $wpdb->dbhost;
+			// $plan = pms_get_subscription_plan($data['subscription_plan_id']);
+			// $ahqPlan = $this->getPlan($plan->name);
+			try {
+				$pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);
+				$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			} catch (PDOException $e) {
+				die("Database connection failed: " . $e->getMessage());
+			}
+			$query = "SELECT * FROM ahq_plans order by id desc";
+			$stmt = $pdo->prepare($query);
+			$stmt->execute();
+
+			$pms_plans = pms_get_subscription_plans();
+			$indexedPlans = array();
+			foreach ($pms_plans as $plan) {
+				if ($plan instanceof PMS_Subscription_Plan) {
+					$indexedPlans[$plan->name][] = array("id"=>$plan->id, "name"=>$plan->name, "duration"=> $plan->duration, "duration_unit" => $plan->duration_unit, "price" => $plan->price );
+				}
+			}
+			print_r($indexedPlans);
+			die('');
+			$plans = $stmt->fetchAll(PDO::FETCH_OBJ);?>
+			<div class="plansContainer">
 					<div class="plansWrapper">
+			
+			<?php foreach ($plans as $plan) {
+
+				?>
+				<div class="planSection">
+					<h1><?php echo($plan->plan_name) ?></h1>
+					<h1 class="price">$<span class="main-price">9.</span><span class="cents">99</span> /mo</h1>
+					<select class="plans_months_select">
+						<option>1 شهر</option>
+						<option>3 أشهر</option>
+						<option>6 أشهر</option>
+						<option>1 سنة</option>
+					</select>
+					<div class="planContent">
+						<h3 class="planLine">بدون إعلانات</h3>
+						<h3 class="planLine">مشاهدة أونلاين</h3>
+						<h3 class="planLine">تحميل</h3>
+						<h3 class="planLine">4 شاشات</h3>
+						<h3 class="planLine">أعلى جودة 4k</h3>
+						<h3 class="planLine">بدون تجديد آلي</h3>
+						<a href="#" class="plan_sub"><span>إشتراك </span> <span>$9.99</span></a>
+					</div>
+				</div>
+			
+			<?php } ?>
+
+				
+				
 						
 						
-						<div class="planSection">
-							<h1>Sama</h1>
-							<h1 class="price">$<span class="main-price">9.</span><span class="cents">99</span> /mo</h1>
-							<div class="planContent">
-								<h3 class="planLine">بدون إعلانات</h3>
-								<h3 class="planLine">مشاهدة + تحميل</h3>
-								<h3 class="planLine">4 شاشات</h3>
-								<h3 class="planLine">جودة 4k</h3>
-								<h3 class="planLine">بدون تجديد آلي</h3>
-							</div>
-						</div>
-						<div class="planSection">
-							<h1>Sensei</h1>
-							<h1 class="price">$<span class="main-price">4.</span><span class="cents">99</span> /mo</h1>
-							<div class="planContent">
-							<h3 class="planLine">بدون إعلانات</h3>
-								<h3 class="planLine">مشاهدة + تحميل</h3>
-								<h3 class="planLine">2 شاشات</h3>
-								<h3 class="planLine">جودة 1080p</h3>
-								<h3 class="planLine">بدون تجديد آلي</h3>
-							</div>
-						</div>
-						<div class="planSection">
-							<h1>Senpai</h1>
-							<h1 class="price">$<span class="main-price">2.</span><span class="cents">99</span> /mo</h1>
-							<div class="planContent">
-								<h3 class="planLine">بدون إعلانات</h3>
-								<h3 class="planLine">مشاهدة فقط</h3>
-								<h3 class="planLine">شاشة واحدة</h3>
-								<h3 class="planLine">جودة 720p</h3>
-								<h3 class="planLine">بدون تجديد آلي</h3>
-								<a href="#">أشتراك</a>
-							</div>
-						</div>
+						
+						
 					</div>
 				</div>
 			<?php
